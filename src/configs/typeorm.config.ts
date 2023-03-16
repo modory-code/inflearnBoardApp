9de419cd80b68ee1144db4099a8826d4ab9c8315
modory-code <modory.code@gmail.com>
2023-03-16 15:28:31 +0900

    TypeORM entities 경로 자동 인식되도록 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,5 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Board } from 'src/boards/board.entity';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -8,6 +7,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: '210621',
   database: 'board-app',
-  entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
+  autoLoadEntities: true,
 };
