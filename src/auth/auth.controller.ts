22f687f7c47df61bb754081bbff5146a2e4da2ed
modory-code <modory.code@gmail.com>
2023-03-18 12:22:34 +0900

    유저 유효성 체크

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
-        return this.authService.signUp(authCredentialDto);
-    }
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
+  }
 }
